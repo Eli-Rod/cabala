3e47ac31c2288cba9ec15cbcaafecd4a12cbe4c2
Eli <48494895+Eli-Rod@users.noreply.github.com>
2023-03-28 22:08:45 -0300

    Criando tabNavigation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { View, Text } from 'react-native';
 
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from 'react-native-animatable';
 import {
   useNavigation,
   // NavigationContainer
 } from "@react-navigation/native";
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-// import { createDrawerNavigator } from '@react-navigation/drawer'
+import Welcome from '../Welcome';
+import SignIn from '../Signin';
+
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { NavigationContainer } from '@react-navigation/native';
 
 import styles from './styles';
 
-export default function SignIn() {
+export default function Home() {
   const navigation = useNavigation();
   // const Drawer = createDrawerNavigator();
+  // const Tab = createMaterialBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
     <View style={styles.container}>
@@ -24,6 +32,11 @@ export default function SignIn() {
       >
         <Text style={styles.message}>Home</Text>
       </Animatable.View>
+
+      {/* <Tab.Navigator >
+        <Tab.Screen name="SignIn" component={SignIn} options={{tabBarStyle: { display: "none" }}} />
+        <Tab.Screen name="Welcome" component={Welcome} options={{tabBarStyle: { display: "none" }}} />
+      </Tab.Navigator> */}
 
       {/* <NavigationContainer>
         <Drawer.Navigator>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,73 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from '@react-navigation/native'
 
 import Welcome from '../pages/Welcome';
 import SignIn from '../pages/Signin';
 import Home from '../pages/Home';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Routes() {
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Welcome"
-        component={Welcome}
-        options={{ 
-          headerShown: false
-         }}
+    <>
+      <Tab.Navigator >
+        <Tab.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            tabBarStyle: { display: "none" },
+            headerShown: false,
+          }}
         />
 
-      <Stack.Screen
-        name="SignIn"
-        component={SignIn}
-        options={{ 
-          headerShown: false
-         }}
-      />
+        <Tab.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{
+            tabBarStyle: { display: "none" },
+            headerShown: false,
+          }}
+        />
 
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ 
-          headerShown: false
-         }}
-      />
-    </Stack.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarStyle: { display: "flex" },
+            headerShown: false
+          }}
+        />
+      </Tab.Navigator>
+
+
+      {/* <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false
+          }}
+        />
+
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{
+            headerShown: false
+          }}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator> */}
+    </>
   )
 }
