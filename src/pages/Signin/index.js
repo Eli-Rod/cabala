0f50f6ada05df39741fbe83db3eae7a0a3713001
Eli <48494895+Eli-Rod@users.noreply.github.com>
2023-03-30 10:10:04 -0300

    Criando autenticao Google e linkando projeto ao expo

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import * as AuthSession from 'expo-auth-session';
+
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import * as Animatable from 'react-native-animatable'
@@ -8,6 +10,23 @@ import styles from './styles';
 
 export default function SignIn() {
   const navigation = useNavigation();
+
+  async function handleGoogleSignIn() {
+    try {
+      const CLIENT_ID = '1059005890895-bglff5kmuad8682t7thos4jun02ahs1d.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@eli.rod/cabalaapp';
+      const SCOPE = encodeURI('profile email');
+      const RESPONSE_TYPE = 'token';
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}$scope=${SCOPE}`;
+      console.log({ authUrl })
+      const authSessionGoogle = await AuthSession.startAsync({ authUrl });
+      console.log(' ================Resultado requisição ================')
+      console.log({ authSessionGoogle })
+    } catch (error) {
+      console.log('Session Google não conectada: ' + error);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -38,7 +57,10 @@ export default function SignIn() {
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Home')}
+          onPress={() => {
+            // navigation.navigate('Home'),
+            handleGoogleSignIn()
+          }}
         >
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
